fefd519287c2901783816a66b306d6f670ad2c53
Oliver <mustardrr4@gmail.com>
2026-08-20 02:26:40 +0100

    test(auth): fix Jest root and environment setup

--- apps/api/jest.config.cjs
+++ apps/api/jest.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json'],
-  roots: ['<rootDir>/apps/api/test'],
+  roots: ['<rootDir>/test'],
+  setupFiles: ['<rootDir>/test/setup-env.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
