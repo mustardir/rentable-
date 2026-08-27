62e27af65f70322c3f6123fa6f2535aa46ee5521
Oliver <mustardrr4@gmail.com>
2026-08-27 19:36:40 +0100

    feat(document-core): export DOCX adapter

--- packages/document-core/src/index.ts
+++ packages/document-core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './docx';
+export * from './docx-renderer';
 export * from './templates';
 export * from './templates/investment-confirmation';
 export * from './validation';
