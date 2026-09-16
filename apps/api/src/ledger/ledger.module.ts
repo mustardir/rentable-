ec1190f9c18bd570f762112beb88e22e93a788ed
Oliver <mustardrr4@gmail.com>
2026-09-16 17:53:50 +0100

    feat(ledger): register account mapping service

--- apps/api/src/ledger/ledger.module.ts
+++ apps/api/src/ledger/ledger.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaLedgerRepository } from './prisma-ledger.repository';
 import { LedgerController } from './ledger.controller';
 import { LedgerService } from './ledger.service';
+import { LedgerAccountMappingService } from './ledger-account-mapping.service';
 
 @Module({
   controllers: [LedgerController],
-  providers: [PrismaLedgerRepository, LedgerService],
-  exports: [PrismaLedgerRepository, LedgerService],
+  providers: [PrismaLedgerRepository, LedgerService, LedgerAccountMappingService],
+  exports: [PrismaLedgerRepository, LedgerService, LedgerAccountMappingService],
 })
 export class LedgerModule {}
